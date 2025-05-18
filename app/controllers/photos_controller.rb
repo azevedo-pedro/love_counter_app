88d8f2a1e40577569189b26d16a3383ce6e64d99
Pedro Azevedo <azevedodev@outlook.com>
2025-05-18 17:05:26 -0300

    feat: update photos controller permisions

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -65,6 +65,6 @@ class PhotosController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def photo_params
-      params.expect(photo: [ :title, :relationship_id ])
+      params.expect(photo: [ :title, :relationship_id, :image ])
     end
 end
