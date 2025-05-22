ee6af4f82748194ce49a359c3782f58c58d4c7f6
Pedro Azevedo <azevedodev@outlook.com>
2025-05-22 20:18:29 -0300

    feat: add new schema and permitions on photos

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -2,4 +2,5 @@ class Photo < ApplicationRecord
   belongs_to :relationship
   has_one_attached :image
   has_many :comments, dependent: :destroy
+  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_05_18_200052) do
+ActiveRecord::Schema[8.0].define(version: 2025_05_22_225847) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -40,6 +40,16 @@ ActiveRecord::Schema[8.0].define(version: 2025_05_18_200052) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.text "body"
+    t.bigint "user_id", null: false
+    t.bigint "photo_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["photo_id"], name: "index_comments_on_photo_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "photos", force: :cascade do |t|
@@ -72,6 +82,8 @@ ActiveRecord::Schema[8.0].define(version: 2025_05_18_200052) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "comments", "photos"
+  add_foreign_key "comments", "users"
   add_foreign_key "photos", "relationships"
   add_foreign_key "relationships", "users"
 end
