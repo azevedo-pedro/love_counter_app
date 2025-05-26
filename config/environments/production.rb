67ac31c564cb5b3f16e392fbd5c7fcfc56046fdf
Pedro Azevedo <azevedodev@outlook.com>
2025-05-26 18:27:29 -0300

    feat: add domains into application

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,9 +80,8 @@ Rails.application.configure do
   config.active_record.attributes_for_inspect = [ :id ]
 
   # Enable DNS rebinding protection and other `Host` header attacks.
-  config.hosts = [
-    "love_counter_app.onrender.com",     # Allow requests from example.com
-  ]
+  config.hosts << "love-counter-app.onrender.com"
+  config.hosts << "falbo.azevedo.dev"
   #
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
